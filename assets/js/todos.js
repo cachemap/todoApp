2d28d8fd454d4da8c9de87bc605467ed9fc5e657
Dillon Notz <dillon.notz@gmail.com>
2018-11-05 17:48:40 -0500

    Add CSS styling to improve look; Implement list item removal functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,30 @@
 // "Check-off" functionality
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
+
+// Click on "X" to delete todo item
+$("ul").on("click", "span", function(event) {
+	// Prevent event bubbling	
+	event.stopPropagation();
+
+	// Fade out list item cleanly
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove(); // Remove item from the DOM
+	});
+})
+
+// Adding new list items
+$("input[type='text']").keypress(function(event) {
+	// If enter is pressed
+	if(event.which === 13) {
+		// Grab new todo text from input
+		var todoText = $(this).val();
+
+		// Remove text from input box
+		$(this).val("");
+
+		// Create a new li containing todoText
+		$("ul").append("<li> <span>X</span> " + todoText + "</li>")
+	}
+})
